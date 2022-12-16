8bc574525aaa7cef7418ccb0ba16000d1acfe2a0
Abdul Basit <106882008+adbul-basit@users.noreply.github.com>
2022-12-16 21:30:44 +0500

    Update Searching.java

--- Searching.java
+++ Searching.java
@@ -2,7 +2,7 @@ package Array;
 
 import java.util.Scanner;
 
-public class LenearSearching {
+public class LinearSearching {
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
 
