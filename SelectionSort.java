aced069eddefaee9d5dac37994e9769a24a90900
Abdul Basit <106882008+adbul-basit@users.noreply.github.com>
2022-12-16 21:30:09 +0500

    Add files via upload

---
+++ SelectionSort.java
@@ -0,0 +1,27 @@
+package Array;
+public class SelectionSort {
+    public static void main(String args[]) {
+        int array[] = {100, 92, 8, 49, 0, 81, 833, 41};
+        selectionSort(array);
+        System.out.println("Sorted array");
+        // Printing Array.
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void selectionSort(int arr[]) {
+        int n = arr.length;
+        for (int i = 0; i < n - 1; i++) {
+            int min = i;
+            for (int j = i + 1; j < n; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                    int temp = arr[min];
+                    arr[min] = arr[i];
+                    arr[i] = temp;
+                }
+            }
+        }
+    }
+}
